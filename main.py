db8f449be2ce66a1ca3eb4deca210da971368438
saravanaselvam266-spsm <saravana.selvam266@gmail.com>
2025-12-14 12:46:47 +0530

    CORSMiddleware created

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from fastapi import FastAPI 
-from db.database import Base , engine
+from fastapi import FastAPI
+from db.database import Base, engine
 from router.user_router import router as user_router
 from router.booking_router import router as booking_router
 from router.location_router import router as location_router
 from router.payment_router import router as payment_router
-from router.service_router import router as service_router 
+from router.service_router import router as service_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
-Base.metadata.create_all(bind = engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_header=["*"],
+)
 
 app.include_router(user_router)
 app.include_router(booking_router)
