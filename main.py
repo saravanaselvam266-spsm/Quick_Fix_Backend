d5eb35dc98b625c1fbb5bde0c41a061116bce3e8
saravanaselvam266-spsm <saravana.selvam266@gmail.com>
2025-12-31 12:11:40 +0530

    location table removed

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from db.database import Base, engine
 from router.user_router import router as user_router
 from router.booking_router import router as booking_router
-from router.location_router import router as location_router
 from router.payment_router import router as payment_router
 from router.service_router import router as service_router
 from fastapi.middleware.cors import CORSMiddleware
@@ -16,11 +15,10 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"], 
+    allow_headers=["*"],
 )
 
 app.include_router(user_router)
 app.include_router(booking_router)
 app.include_router(payment_router)
 app.include_router(service_router)
-app.include_router(location_router)
